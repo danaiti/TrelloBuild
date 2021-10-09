26904ce5bc6728b726cf23f39332a442331879dc
danaiti <tranthedan1609@gmail.com>
2021-10-09 19:39:56 +0700

    build

---
+++ build/exe.win32-3.7/lib/getTrelloData.py
@@ -0,0 +1,87 @@
+from trello import TrelloClient, board
+import pandas as pd
+import requests
+from lib import uploadTrelloData as ul
+
+API_KEY = ""
+TOKEN = ""
+all_boards = None
+client = None
+
+def getKey():
+    global API_KEY
+    global TOKEN
+    global client
+
+    with open("lib/data.txt", "r") as f1:
+        contents1 = f1.read()
+        if "-" not in contents1:
+            f1.close()
+            return 1
+        API_KEY = contents1.split("-")[0]
+        TOKEN = contents1.split("-")[1]
+
+        client = TrelloClient(
+            api_key = API_KEY,
+            token = TOKEN
+    )
+    f1.close()
+
+def getAllBoard(client):
+    global all_boards
+    try:
+        all_boards = client.list_boards()
+        print(type(all_boards))
+        return 0
+    except Exception as e:
+        return 1
+
+def create_list(list_name):
+    getKey()
+    global client
+    url = f"https://api.trello.com/1/boards/{board_id}/lists"
+    querystring = {"name": list_name, "key": API_KEY, "token": TOKEN}
+    response = requests.request("POST", url, params=querystring)
+    list_id = response.json()["id"]
+    return list_id
+
+def getData():
+    getKey()
+    global all_boards
+    global client
+    if getAllBoard(client) == 1:
+        return 1
+    else:
+        nameList = []
+        nameCard = []
+        attach = []
+        try:
+            for myboard in all_boards:
+                name = myboard.name
+                if "_ignore" not in name:
+                    print ("Export from board: " + name)
+                    for list in myboard.all_lists():
+                        for card in list.list_cards():
+                            #here
+                            hasAtt = False
+                            for atc in card.attachments:
+                                hasAtt = True
+                                attach.append(atc.get('url'))
+                            if(hasAtt):
+                                nameCard.append(card.name)
+                                nameList.append(list.name)
+                else:
+                    print ("===> Ignore")
+                    continue
+            df = pd.DataFrame({'Product Name':nameCard, 'Img': attach, 'Product Type': nameList})
+            df.to_csv('products.csv', index=False, encoding='utf-8')
+        except Exception as e2:
+            print (e2)
+            return 2
+        return 0
+
+def uploadData(fileName,idBoard):
+    getKey()
+    return ul.readDataFromCsv(fileName,idBoard,API_KEY, TOKEN)
+
+
---
+++ build/exe.win32-3.7/lib/uploadTrelloData.py
@@ -0,0 +1,67 @@
+from datetime  import datetime
+import requests
+from csv import reader
+def create_board(key, token):
+
+    now = datetime.now()
+    current_time = now.strftime("%H%M%S")
+    board_name = "SanPham" + current_time
+    print(board_name)
+    url = "https://api.trello.com/1/boards/"
+    querystring = {"name": board_name, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    print (response.json()["shortUrl"])
+    board_id = response.json()["shortUrl"].split("/")[-1].strip()
+    return board_id
+
+def create_list(key, token ,board_id, list_name):
+    url = f"https://api.trello.com/1/boards/{board_id}/lists"
+    querystring = {"name": list_name, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    list_id = response.json()["id"]
+    return list_id
+
+def create_card(key, token, list_id, card_name):
+    url = f"https://api.trello.com/1/cards"
+    querystring = {"name": card_name, "idList": list_id, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    card_id = response.json()["id"]
+    return card_id
+
+def uploadAttach(key, token, card_id, img_url):
+    try:
+        querystring = {"key": key, "token": token}
+        files = {
+            'file': (img_url, open(img_url, 'rb')),
+        }
+
+        requests.post('https://api.trello.com/1/cards/%s/attachments' % card_id, params=querystring, files=files)
+    except Exception as e:
+        print('Loi attach')
+
+
+def readDataFromCsv(fileName,idBoard, key, token):
+    try:
+        if idBoard == '':
+            board_id = create_board(key, token)
+        else:
+            board_id = idBoard
+        with open(fileName, "r") as f1:
+            csv_reader = reader(f1)
+            listName = 'fakeList'
+            cardName = 'fakeCard'
+            for row in csv_reader:
+                if row[0] != '' and row[1]!='':
+                    listName = row[0]
+                    list_id = create_list(key, token, board_id, listName)
+                    card_id = create_card(key, token, list_id, row[1])
+                    if (row[2] != ''):
+                        uploadAttach(key, token, card_id, row[2])
+                elif row[1] != '':
+                    card_id = create_card(key, token, list_id, row[1])
+                    if (row[2] != ''):
+                        uploadAttach(key, token, card_id, row[2])
+        return board_id
+    except Exception as e:
+        return "err"
+
